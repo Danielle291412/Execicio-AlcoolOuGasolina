646d6fc13bfc82a6ab9fee88ee78c137677c5f9d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-01 22:23:00 -0300

    mostrar ao usuário se é recomendado ele utilizar álcool ou gasolina

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.WriteLine("Alcool ou Gasolina?\n");
+
+Console.Write("Determine o valor do álcool (R$):");
+decimal Álcool = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Determine o valor da gasolina (R$):");
+decimal Gasolina = Convert.ToDecimal(Console.ReadLine());
+
+string recomendado;
+
+double razao = RazaoÁlcoolGasolina(Álcool, Gasolina);
+
+if (AbastecerComGasolina(razao))
+{
+    recomendado = "Gasolina";
+}
+
+else
+{
+    recomendado = "Álcool";
+}
+
+
+Console.WriteLine($"\nO valor do álcool corresponde a {razao:P1} do valor da gasolina.");
+Console.WriteLine($"\nRecomendado: Abastecer com {recomendado.ToUpper()}.");
+
+double RazaoÁlcoolGasolina(decimal valorÁlcool, decimal valorGasolina)
+{
+    return Convert.ToDouble(valorÁlcool / valorGasolina);
+}
+
+bool AbastecerComGasolina(double razao)
+{
+    const double MinRazaoUsoGasolina = 0.73;
+    return razao >= MinRazaoUsoGasolina;
+}
